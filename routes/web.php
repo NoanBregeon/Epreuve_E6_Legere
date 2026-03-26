3b212e2268a9cd261edb275193a30de161ddaa71
NoanBregeon <furienocturne2.0@gmail.com>
2026-03-26 10:26:12 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -50,16 +50,6 @@ Route::middleware('auth')->group(function () {
 Route::get('/commande/{id}', [CommandeController::class, 'show'])->name('commande.show'); // Détail commande
 Route::get('/commande/confirmation/{id}', [CommandeController::class, 'confirmation'])->name('commande.confirmation');
 
-// Admin (nécessite auth + role admin via Bouncer ou middleware)
-Route::prefix('admin')->middleware(['auth', \App\Http\Middleware\AdminMiddleware::class])->group(function () {
-    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
 
-    // Gestion des produits
-    Route::get('/produits/create', [ProduitsController::class, 'create'])->name('produits.create');
-    Route::post('/produits', [ProduitsController::class, 'store'])->name('produits.store');
-    Route::get('/produits/{produit}/edit', [ProduitsController::class, 'edit'])->name('produits.edit');
-    Route::put('/produits/{produit}', [ProduitsController::class, 'update'])->name('produits.update');
-    Route::delete('/produits/{produit}', [ProduitsController::class, 'destroy'])->name('produits.destroy');
-});
 
 require __DIR__.'/auth.php';
